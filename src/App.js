9d8a3bfb2869c26ee674a7bebbe9190c50f9058c
MatthewLau14 <mlau191@uw.edu>
2024-10-13 04:18:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './Components/HomePage/HomePage'; // Create this component
-import SeattlePage from './Components/SeattlePage/SeattlePage';
+import HomePage from './Components/HomePage/HomePage'; 
+import SeattlePage from './Components/SeattlePage/SeattlePage'; 
 import RentonPage from './Components/RentonPage/RentonPage';
 import BellevuePage from './Components/BellevuePage/BellevuePage';
 
@@ -16,10 +16,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginSignup />} /> {/* This should render the LoginSignup component */}
+        <Route path="/" element={<LoginSignup />} /> {/* Renders the LoginSignup component */}
         <Route path="/home" element={<HomePage />} />
-      </Routes>
-      <Routes>
         <Route path="/seattle" element={<SeattlePage />} />
         <Route path="/renton" element={<RentonPage />} />
         <Route path="/bellevue" element={<BellevuePage />} />
